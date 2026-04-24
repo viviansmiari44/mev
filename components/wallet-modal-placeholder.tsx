3d565cf97c5fd012863dfe2c1ab893a598561283
mike clinton <mikeclinton508@gmail.com>
2026-04-24 07:03:13 +0100

    fixing the deeplink

--- components/wallet-modal-placeholder.tsx
+++ components/wallet-modal-placeholder.tsx
@@ -45,7 +45,7 @@ export default function WalletModalPlaceholder({ trigger }: { trigger: ReactNode
      name: "SafePal", 
       icon: "/wallets/safepalw.png", 
       // 🟡 Alternative: Direct custom scheme with the open_url command
-      href: `safepalwallet://dapp?url=${ENCODED_URL}`
+      href: `safepalwallet://dapp${ENCODED_URL}`
     },
     { 
       name: "TokenPocket", 
